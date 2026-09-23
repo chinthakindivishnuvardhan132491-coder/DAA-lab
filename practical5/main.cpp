957f6612b772ef4508fadc8812ed30cd6943a11b
chinthakindivishnuvardhan132491-coder <chinthakindivishnuvardhan.132491@marwadiuniversity.ac.in>
2026-09-23 16:05:39 +0530

    Add files via upload

---
+++ practical5/main.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n, W;
+
+    cout << "Enter number of items: ";
+    cin >> n;
+
+    int weight[n], value[n];
+
+    cout << "Enter weights of items:\n";
+    for (int i = 0; i < n; i++) {
+        cin >> weight[i];
+    }
+
+    cout << "Enter values of items:\n";
+    for (int i = 0; i < n; i++) {
+        cin >> value[i];
+    }
+
+    cout << "Enter capacity of knapsack: ";
+    cin >> W;
+
+    // DP table
+    int dp[n + 1][W + 1];
+
+    // Initialize DP table
+    for (int i = 0; i <= n; i++) {
+        for (int w = 0; w <= W; w++) {
+
+            if (i == 0 || w == 0) {
+                dp[i][w] = 0;
+            }
+            else if (weight[i - 1] <= w) {
+                dp[i][w] = max(
+                    value[i - 1] + dp[i - 1][w - weight[i - 1]],
+                    dp[i - 1][w]
+                );
+            }
+            else {
+                dp[i][w] = dp[i - 1][w];
+            }
+        }
+    }
+
+    cout << "\nMaximum value = " << dp[n][W] << endl;
+
+    cout<<endl<<"Enrollment No: 92460118608"<<endl;
+
+    return 0;
+}
